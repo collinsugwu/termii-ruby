aabbbe6c1f67c55bdea2e4afe199e311176ecc99
collinsugwu <collinsuchinaka@gmail.com>
2023-05-12 19:31:45 +0100

    setup rspec

--- spec/termii_ruby/termii_sender_spec.rb
+++ spec/termii_ruby/termii_sender_spec.rb
@@ -1,11 +1,11 @@
 # frozen_string_literal: true
 
 RSpec.describe TermiiRuby::TermiiSender do
-    it "has a version number" do
-      expect(TermiiRuby::VERSION).not_to be nil
-    end
-  
-    it "does something useful" do
-      expect(false).to eq(false)
-    end
+  it "has a version number" do
+    expect(TermiiRuby::VERSION).not_to be nil
+  end
+
+  it "does something useful" do
+    expect(false).to eq(false)
+  end
 end
